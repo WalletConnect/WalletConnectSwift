cd006999340b3badafd964a9876ded4c19574a0b
Andrey Scherbovich <andrey@gnosis.pm>
2019-07-02 18:35:43 +0200

    GH-917 finish Serializer

--- WalletConnect/Serializer.swift
+++ WalletConnect/Serializer.swift
@@ -5,16 +5,34 @@
 import Foundation
 
 
+/// We do not expect incomming responses as requests that we send are notifications.
 protocol ResponseSerializer {
 
+    /// Serialize WalletConnect Response into text message.
+    ///
+    /// - Parameter response: Response object
+    /// - Returns: text message
+    /// - Throws: serialization errors
     func serialize(_ response: Response) throws -> String
-    func deserialize(_ text: String, url: WCURL) throws -> Response
 
 }
 
 protocol RequestSerializer {
 
+    /// Serialize WalletConnect Request into text message.
+    ///
+    /// - Parameter request: Request object
+    /// - Returns: text message
+    /// - Throws: serialization errors
     func serialize(_ request: Request) throws -> String
+
+    /// Deserialize incoming WalletConnet text message.
+    ///
+    /// - Parameters:
+    ///   - text: encoded text message
+    ///   - url: WalletConnect session URL data (required for text decoding).
+    /// - Returns: request object
+    /// - Throws: deserialization errors
     func deserialize(_ text: String, url: WCURL) throws -> Request
 
 }
@@ -29,44 +47,33 @@ protocol Codec {
 class JSONRPCSerializer: RequestSerializer, ResponseSerializer {
 
     private let codec: Codec = AES_256_CBC_HMAC_SHA256_Codec()
-    private let pubSubAdapter: PubSubAdapter = PubSubAdapter()
+    private let pubSubAdapter = PubSubAdapter()
     private let jsonrpc = JSONRPCAdapter()
 
+    // MARK: - RequestSerializer
+
     func serialize(_ request: Request) throws -> String {
-        let json = try jsonrpc.json(from: request)
-        let cipherText = try codec.encode(plainText: json.string, key: request.origin.key)
+        let jsonText = try jsonrpc.json(from: request)
+        let cipherText = try codec.encode(plainText: jsonText, key: request.origin.key)
         let message = PubSubAdapter.Message(topic: request.origin.topic, type: .pub, payload: cipherText)
         let result = try pubSubAdapter.string(from: message)
         return result
     }
 
-    /// Deserialize incoming WalletConnet text message.
-    ///
-    /// - Parameters:
-    ///   - text: encoded text messafe
-    ///   - url: WalletConnect session URL data (required for text decoding).
-    /// - Returns: request object
-    /// - Throws: deserialization errors
     func deserialize(_ text: String, url: WCURL) throws -> Request {
         let message = try pubSubAdapter.message(from: text)
-        let JSONRPCPayloadText = try codec.decode(cipherText: message.payload, key: url.key)
-        let result = try jsonrpc.request(from: JSONRPC_2_0.JSON(JSONRPCPayloadText), origin: url)
+        let payloadText = try codec.decode(cipherText: message.payload, key: url.key)
+        let result = try jsonrpc.request(from: payloadText, origin: url)
         return result
     }
+
+    // MARK: - ResponseSerializer
 
     func serialize(_ response: Response) throws -> String {
-        let json = try jsonrpc.json(from: response)
-        let cipherText = try codec.encode(plainText: json.string, key: response.destination.key)
+        let jsonText = try jsonrpc.json(from: response)
+        let cipherText = try codec.encode(plainText: jsonText, key: response.destination.key)
         let message = PubSubAdapter.Message(topic: response.destination.topic, type: .pub, payload: cipherText)
         let result = try pubSubAdapter.string(from: message)
-        return result
-    }
-
-    func deserialize(_ text: String, url: WCURL) throws -> Response {
-        let message = try pubSubAdapter.message(from: text)
-        let plainText = try codec.decode(cipherText: message.payload, key: url.key)
-        let result = try jsonrpc.response(from: JSONRPC_2_0.JSON(plainText))
-//        result.setOrigin(url)
         return result
     }
 
@@ -74,29 +81,17 @@ class JSONRPCSerializer: RequestSerializer, ResponseSerializer {
 
 class JSONRPCAdapter {
 
-    enum CodableError: Error {
-        case JSONstringToRequestFailed
-        case requestToJSONstringFailed
+    func json(from request: Request) throws -> String {
+        return try request.payload.json().string
     }
 
-    func json(from: Request) throws -> JSONRPC_2_0.JSON {
-        preconditionFailure()
+    func request(from json: String, origin: WCURL) throws -> Request {
+        let JSONRPCRequest = try JSONRPC_2_0.Request.create(from: JSONRPC_2_0.JSON(json))
+        return Request(payload: JSONRPCRequest, origin: origin)
     }
 
-    func request(from json: JSONRPC_2_0.JSON, origin: WCURL) throws -> Request {
-        guard let data = json.string.data(using: .utf8) else {
-            throw DataConversionError.stringToDataFailed
-        }
-        let JSONRPC_request = try JSONDecoder().decode(JSONRPC_2_0.Request.self, from: data)
-        return Request(payload: JSONRPC_request, origin: origin)
-    }
-
-    func json(from: Response) throws -> JSONRPC_2_0.JSON {
-        preconditionFailure()
-    }
-
-    func response(from: JSONRPC_2_0.JSON) throws -> Response {
-        preconditionFailure()
+    func json(from response: Response) throws -> String {
+        return try response.payload.json().string
     }
 
 }
