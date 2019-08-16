4fd48b9f3b893e336ce57a24fe519a1885881727
Andrey Scherbovich <andrey.scherbovich@gmail.com>
2019-08-16 12:11:00 +0200

    add missing WalletConnect methods

--- Sources/Client.swift
+++ Sources/Client.swift
@@ -4,13 +4,6 @@
 
 import Foundation
 
-/*
- WC API:
- eth_sendTransaction
- eth_signTransaction
- eth_sendRawTransaction
- */
-
 public protocol ClientDelegate: class {
 
     func client(_ client: Client, didFailToConnect url: WCURL)
@@ -69,7 +62,7 @@ public class Client: WalletConnect {
     ///   - url: WalletConnect url object.
     ///   - message: String representin Data to sign.
     ///   - account: String representing Ethereum address.
-    ///   - completion: String representing signature.
+    ///   - completion: Response with string representing signature, or error.
     /// - Throws: client error.
     public func personal_sign(url: WCURL,
                               message: String,
@@ -85,7 +78,7 @@ public class Client: WalletConnect {
     ///   - url: WalletConnect url object.
     ///   - account: String representing Ethereum address.
     ///   - message: String representin Data to sign.
-    ///   - completion: String representing signature.
+    ///   - completion: Response with string representing signature, or error.
     /// - Throws: client error.
     public func eth_sign(url: WCURL,
                          account: String,
@@ -101,7 +94,7 @@ public class Client: WalletConnect {
     ///   - url: WalletConnect url object.
     ///   - account: String representing Ethereum address.
     ///   - message: String representin Data to sign.
-    ///   - completion: String representing signature.
+    ///   - completion: Response with string representing signature, or error.
     /// - Throws: client error.
     public func eth_signTypedData(url: WCURL,
                                   account: String,
@@ -117,6 +110,65 @@ public class Client: WalletConnect {
                       completion: @escaping RequestResponse) throws {
         let payload = JSONRPC_2_0.Request(method: method,
                                           params: .positional([.string(param1), .string(param2)]),
+                                          id: .string(UUID().uuidString))
+        let request = Request(payload: payload, url: url)
+        try send(request, completion: completion)
+    }
+
+    /// https://docs.walletconnect.org/json-rpc/ethereum#eth_sendtransaction
+    /// Request to send a transaction.
+    ///
+    /// - Parameters:
+    ///   - url: WalletConnect url object.
+    ///   - transaction: Transaction object.
+    ///   - completion: Response with string representing transaction hash, or error.
+    /// - Throws: client error.
+    public func eth_sendTransaction(url: WCURL,
+                                    transaction: Transaction,
+                                    completion: @escaping RequestResponse) throws {
+        try handleTransaction(url: url, method: "eth_sendTransaction", transaction: transaction, completion: completion)
+    }
+
+    /// https://docs.walletconnect.org/json-rpc/ethereum#eth_signtransaction
+    /// Request to sign a transaction.
+    ///
+    /// - Parameters:
+    ///   - url: WalletConnect url object.
+    ///   - transaction: Transaction object.
+    ///   - completion: Response with string representing transaction signature, or error.
+    /// - Throws: client error.
+    public func eth_signTransaction(url: WCURL,
+                                    transaction: Transaction,
+                                    completion: @escaping RequestResponse) throws {
+        try handleTransaction(url: url, method: "eth_signTransaction", transaction: transaction, completion: completion)
+    }
+
+    private func handleTransaction(url: WCURL,
+                                   method: String,
+                                   transaction: Transaction,
+                                   completion: @escaping RequestResponse) throws {
+        let requestParamsData = try JSONEncoder().encode([transaction])
+        let requestParams = try JSONDecoder().decode(JSONRPC_2_0.Request.Params.self, from: requestParamsData)
+        let payload = JSONRPC_2_0.Request(method: method,
+                                          params: requestParams,
+                                          id: .string(UUID().uuidString))
+        let request = Request(payload: payload, url: url)
+        try send(request, completion: completion)
+    }
+
+    /// https://docs.walletconnect.org/json-rpc/ethereum#eth_sendrawtransaction
+    /// Request to send a raw transaction. Creates new message call transaction or
+    /// a contract creation for signed transactions.
+    ///
+    /// - Parameters:
+    ///   - url: WalletConnect url object.
+    ///   - data: Data as String.
+    ///   - completion: Response with the transaction hash, or the zero hash if the transaction is not
+    ///                 yet available, or error.
+    /// - Throws: client error.
+    public func eth_sendRawTransaction(url: WCURL, data: String, completion: @escaping RequestResponse) throws {
+        let payload = JSONRPC_2_0.Request(method: "eth_sendRawTransaction",
+                                          params: .positional([.string(data)]),
                                           id: .string(UUID().uuidString))
         let request = Request(payload: payload, url: url)
         try send(request, completion: completion)
@@ -212,6 +264,34 @@ public class Client: WalletConnect {
             queue.sync { [unowned self] in
                 _ = self.responses.removeValue(forKey: requestID)
             }
+        }
+
+    }
+
+    /// https://docs.walletconnect.org/json-rpc/ethereum#parameters-3
+    public struct Transaction: Encodable {
+        var from: String
+        var to: String
+        var data: String?
+        var gasLimit: String?
+        var gasPrice: String?
+        var value: String?
+        var nonce: String?
+
+        public init(from: String,
+                    to: String,
+                    data: String?,
+                    gasLimit: String?,
+                    gasPrice: String?,
+                    value: String?,
+                    nonce: String?) {
+            self.from = from
+            self.to = to
+            self.data = data
+            self.gasLimit = gasLimit
+            self.gasPrice = gasPrice
+            self.value = value
+            self.nonce = nonce
         }
 
     }
