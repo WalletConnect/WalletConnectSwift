706250ea5465b97076b2f39d72befc3cadd883c2
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-08-20 12:09:53 +0200

    GH-6 added carthage packaging test project

---
+++ PackagingTest/Carthage/Carthage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carthage
+//
+//  Created by Dmitry Bespalov on 20.08.19.
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
