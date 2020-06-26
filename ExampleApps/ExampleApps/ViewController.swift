8fa3a064ed3f92ef0b503ab0063e2d9b04ff0090
Andrey Scherbovich <andrey@gnosis.io>
2020-06-26 13:50:49 +0200

    add swift pm dependencies; remove example apps

---
+++ ExampleApps/ExampleApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleApps
+//
+//  Created by Andrey Scherbovich on 26.06.20.
+//  Copyright © 2020 Gnosis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
