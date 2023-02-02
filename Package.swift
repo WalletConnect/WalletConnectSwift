cc88bfba22406be19c10f92e12f3d659480fa9db
Luis Ascorbe <devlascorbe@gmail.com>
2023-02-02 12:12:56 +0100

    Properly update to 1.6.0

--- Package.swift
+++ Package.swift
@@ -9,17 +9,26 @@ let package = Package(
     products: [
         .library(
             name: "WalletConnectSwift",
-            targets: ["WalletConnectSwift"])
+            targets: ["WalletConnectSwift"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.6.0"))
+        .package(
+            url: "https://github.com/krzyzanowskim/CryptoSwift.git",
+            .upToNextMinor(from: "1.6.0")
+        )
     ],
     targets: [
         .target(
-            name: "WalletConnectSwift", 
+            name: "WalletConnectSwift",
             dependencies: ["CryptoSwift"],
-            path: "Sources"),
-        .testTarget(name: "WalletConnectSwiftTests", dependencies: ["WalletConnectSwift"], path: "Tests"),
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "WalletConnectSwiftTests",
+            dependencies: ["WalletConnectSwift"],
+            path: "Tests"
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
