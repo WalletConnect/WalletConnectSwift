9ebd755a7890b62a3e33d53623546d68ec4a09d7
Mouaz <mouaz@safe.global>
2023-06-04 22:20:29 +0200

    Update CryptoSwift package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["WalletConnectSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.5.1"))
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.7.1"))
     ],
     targets: [
         .target(
